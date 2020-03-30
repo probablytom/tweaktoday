13d9e99905e1d752f5299ad872996c08a2c27597
Tom Wallis <github@tomwallis.net>
2020-03-30 11:16:35 +0000

    SSL setup

--- tweaktoday/urls.py
+++ tweaktoday/urls.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path, include, re_path
 from django.conf.urls.static import static
 from django.conf import settings
 
@@ -7,6 +7,7 @@ from django.conf import settings
 urlpatterns = [
     path('', include('tt_core.urls')),
     path('admin/', admin.site.urls),
+    re_path(r'^\'well-known/', include('letsencrypt.urls')),
 ]
 urlpatterns += static(settings.MEDIA_URL,
                       document_root=settings.MEDIA_ROOT)
